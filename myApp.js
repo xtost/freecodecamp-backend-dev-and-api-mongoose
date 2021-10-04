a1dd30b9def00e9da7cd78c17739c3f642068287
Your Name <you@example.com>
2021-10-04 10:03:11 +0200

    array

--- myApp.js
+++ myApp.js
@@ -33,8 +33,8 @@ const createAndSavePerson = (done) => {
 };
 
 //4
-
-let arrayOfPeople = Person.create([{
+/*
+let myArrayOfPeople = [{
   name: 'XaviMany1',
   age: 21,
   favoriteFoods: ['pizza1', 'hotdog1']
@@ -48,11 +48,11 @@ let arrayOfPeople = Person.create([{
     name: 'XaviMany3',
     age: 23,
     favoriteFoods: ['pizza3', 'hotdog3']
-  }]);
+  }];
 
-
+*/
 const createManyPeople = (arrayOfPeople, done) => {
-  arrayOfPeople.save((err, data) => (err ? done(err) : done(null, data)));
+  Person.create((err, data) => (err ? done(err) : done(null, data)));
   // done(null /*, data*/);
 };
 
